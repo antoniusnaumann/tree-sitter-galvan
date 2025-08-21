52aa540047695e4f82874431de659a47298331b6
Antonius Naumann <git@antonius.dev>
2025-08-21 22:19:03 +0200

    Build grammar for abi 14

--- src/parser.c
+++ src/parser.c
@@ -14,7 +14,7 @@
 #pragma GCC optimize ("O0")
 #endif
 
-#define LANGUAGE_VERSION 15
+#define LANGUAGE_VERSION 14
 #define STATE_COUNT 1101
 #define LARGE_STATE_COUNT 12
 #define SYMBOL_COUNT 195
@@ -4909,7 +4909,7 @@ static bool ts_lex_keywords(TSLexer *lexer, TSStateId state) {
   }
 }
 
-static const TSLexerMode ts_lex_modes[STATE_COUNT] = {
+static const TSLexMode ts_lex_modes[STATE_COUNT] = {
   [0] = {.lex_state = 0, .external_lex_state = 1},
   [1] = {.lex_state = 72},
   [2] = {.lex_state = 4, .external_lex_state = 2},
@@ -54450,7 +54450,6 @@ TS_PUBLIC const TSLanguage *tree_sitter_galvan(void) {
     .state_count = STATE_COUNT,
     .large_state_count = LARGE_STATE_COUNT,
     .production_id_count = PRODUCTION_ID_COUNT,
-    .supertype_count = SUPERTYPE_COUNT,
     .field_count = FIELD_COUNT,
     .max_alias_sequence_length = MAX_ALIAS_SEQUENCE_LENGTH,
     .parse_table = &ts_parse_table[0][0],
@@ -54479,13 +54478,6 @@ TS_PUBLIC const TSLanguage *tree_sitter_galvan(void) {
       tree_sitter_galvan_external_scanner_deserialize,
     },
     .primary_state_ids = ts_primary_state_ids,
-    .name = "galvan",
-    .max_reserved_word_set_size = 0,
-    .metadata = {
-      .major_version = 0,
-      .minor_version = 0,
-      .patch_version = 3,
-    },
   };
   return &language;
 }
